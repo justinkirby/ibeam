501971bf4249fd81cae15ca8f5224059283f2c34
Justin Kirby <justin.kirby@trafficmp.com>
2011-03-17 00:15:55 -0400

    ibeam now deals with embedded package and does appropriate sanity checks

--- src/ibeam_cmd_install.erl
+++ src/ibeam_cmd_install.erl
@@ -33,35 +33,41 @@ run() ->
     App = ibeam_config:get_global(name),
     Vsn = ibeam_config:get_global(vsn),
     TmpDir = ibeam_config:get_global(tmp_dir),
-    
-
-    ibeam_utils:hook(TmpDir,{App,Vsn},install_pre,[TmpDir,App,Vsn]),
-
     AppList = ibeam_config:get_global(app_info),
     SysList = ibeam_config:get_global(sys_info),
     Prefix = ibeam_config:get_global(install_prefix),
+    DestRoot = filename:join([Prefix,App]),
+    DestLib = filename:join([DestRoot,"lib"]),
+    
+    ibeam_utils:hook(TmpDir,{App,Vsn},install_pre,[TmpDir,App,Vsn]),
 
-
+    filelib:ensure_dir(DestLib),
     
 
     
 
-%%    ToInstall = install_list(AppList,SysList),
+    ToInstall = install_list(AppList,SysList),
 
-%%    ok = copy_apps(ToInstall),
-%%    ok = copy_releases({App,Vsn}),
+
+    ?INFO("SYSLIST ~n~p~n",[SysList]),
+    ?INFO("APPLIST ~n~p~n",[AppList]),
+    ?INFO("INSTALL list~n~p~n.",[ToInstall]),
+
+    ok = copy_apps(DestLib,ToInstall),
+    ok = copy_releases(DestRoot,{App,Vsn}),
 
     
-    ibeam_utils:hook(code:root_dir(),{App,Vsn},install_post,[code:lib_dir(),App,Vsn]),
-    ?CONSOLE("Install: ~p~n",[ToInstall]),
+    ibeam_utils:hook(DestRoot,{App,Vsn},install_post,[DestLib,App,Vsn]),
 
     ok.
 
 
 install_list(App, Sys) ->
-    %% note we never touch 'sys' apps.  The verify type of full|sys should have been run.
-    %% if you want to make sure the environment is setup, then verify with type=full|sys
-    All = proplists:get_value(dep,App,[]) ++ proplists:get_value(app,App,[]),
+    %% note we check sys apps because there might be nothing there,
+    %% e.g. first install
+    All = proplists:get_value(dep,App,[]) ++
+	proplists:get_value(app,App,[]) ++
+	proplists:get_value(sys,App,[]),
     case ibeam_config:get_global(force,false) of
 	true ->
 	    %% if force is specified, then just install everything
@@ -98,21 +104,22 @@ install_list_nice([{Name,Vsn}|App],Sys,Install) ->
 
     
 	    
-copy_apps(Install) ->
+copy_apps(LibPath,Install) ->
     TmpDir = ibeam_config:get_global(tmp_dir),
     TmpLib = filename:join([TmpDir,"lib"]),
 
     lists:foreach(fun({Name,Vsn}) ->
 			  Src = filename:join([TmpLib,atom_to_list(Name)++"-"++Vsn]),
-			  Dst = code:lib_dir(),
+			  Dst = LibPath,
 			  Res = ibeam_file_utils:cp_r(Src,Dst),
 			  ?CONSOLE("CP res: ~p~n",[Res])			      
 		  end,Install),
 
     ok.
 
-copy_releases({Name,Vsn}) ->
+copy_releases(DestRoot,{Name,Vsn}) ->
     TmpDir = ibeam_config:get_global(tmp_dir),
-    TmpRel = filename:join([TmpDir,"releases",Vsn,"*"]),
-    DstRel = filename:join([code:root_dir(),"releases",Name++"-"++Vsn]),
+    TmpRel = filename:join([TmpDir,"releases",Vsn]),
+    DstRel = filename:join([DestRoot,"releases"]),
+
     ibeam_file_utils:cp_r(TmpRel,DstRel).
