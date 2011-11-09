a8a2abaefc4c89c3feaa52abce3cb9ff57f288ab
Justin Kirby <justin.kirby@trafficmp.com>
2011-11-09 13:39:44 -0500

    ws & load app immediately

--- src/ibeam.erl
+++ src/ibeam.erl
@@ -2,16 +2,18 @@
 -module(ibeam).
 
 -export([
-	 main/1
-	 ]).
+         main/1
+        ]).
 
 main(Args) ->
+    %% we always need to load the app, so just load it here
+    ok = application:load(ibeam),
     case catch(ibeam_core:run(Args)) of
-	ok ->
-	    ok;
-	{error, failed} ->
-	    halt(1);
-	Error ->
-	    io:format("Uncaught error in ibeam_core: ~p~n",[Error]),
-	    halt(1)
+        ok ->
+            ok;
+        {error, failed} ->
+            halt(1);
+        Error ->
+            io:format("Uncaught error in ibeam_core: ~p~n",[Error]),
+            halt(1)
     end.
