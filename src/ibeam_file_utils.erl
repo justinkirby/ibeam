eaef96f6b6053f14589e76f499c279b6779ac8d0
Justin Kirby <justin.kirby@trafficmp.com>
2011-11-23 16:24:15 -0500

    fix for de1359. ibeam now does 'proper' path munging for destinations

--- src/ibeam_file_utils.erl
+++ src/ibeam_file_utils.erl
@@ -6,6 +6,8 @@
 	 cp_r/2
 	 ]).
 
+path_sep() -> filename:join(["a","b"]) -- "ab".
+
 
 
 rm_rf(Target) ->
@@ -19,15 +21,51 @@ rm_rf(Target) ->
 	    ok
     end.
 
-
+%% do NOT use this for renaming!
 cp_r(Sources, Dest) ->
     case os:type() of
         {unix, _} ->
-%%            SourceStr = string:join(Sources, " "),
-            {ok, []} = ibeam_utils:sh(?FMT("cp -fR ~s ~s", [Sources, Dest]),
+            {ok, []} = ibeam_utils:sh(?FMT("cp -fR ~s ~s", [Sources, munge_dest(Sources, Dest)]),
                                       [{use_stdout, false}, return_on_error]),
             ok;
 	OS ->
 	    ?ABORT("Do not know how to cp_r on ~p~n",[OS]),
 	    ok
     end.
+
+%% this is complicated so bear with me.  the cp_r can make nested
+%% dirs.  this means that cp -fR /tmp/foo /opt/foo MAY create
+%% /opt/foo/foo IF the destinations exists and is a dir.
+%%
+%% The solution is to do the following:
+%% - if the last path of the source and destination are the same, e.g. /tmp/foo -> /opt/foo
+%%   - if the dest exists AND is a dir then remove the last path component from dest
+%% /tmp/foo -> /opt/foo  becomes /tmp/foo -> /opt
+
+%% - if the src is a dir and the last path component are not the same,
+%% then append a / to the dest to make sure the path is created.
+
+munge_dest(Source, Dest) ->
+    SrcLast = lists:last(filename:split(Source)),
+    DestLast = lists:last(filename:split(Dest)),
+
+    case SrcLast == DestLast of
+        false ->
+            %% are not the same, then check to see if Source is a dir,
+            %% if so append / to Dest
+            case filelib:is_dir(Source) of
+                false -> Dest;
+                true ->
+                    Dest ++ path_sep()
+            end;
+        true ->
+            case filelib:is_dir(Dest) of
+                false -> Dest;
+                true ->
+                    %% remove the last elem of the list:
+                    filename:join(lists:reverse(tl(lists:reverse(filename:split(Dest)))))
+            end
+    end.
+
+
+
